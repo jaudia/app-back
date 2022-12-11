40fa7e0f448629340eb8dec45b14c38e7ebf1856
Juan <juan.audia@gmail.com>
2022-12-11 19:33:14 -0300

    prueba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = process.env.PORT_BACKEND;
+const port = Number(process.env.PORT_BACKEND);
 require('dotenv').config()
 
 app.use(cors());
