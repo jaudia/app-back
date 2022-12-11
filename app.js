a7b31462a6f8ecf4acf19c86c0a5109854327c0e
Juan <juan.audia@gmail.com>
2022-12-11 19:09:02 -0300

    envio de respuesta al front

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 require('dotenv').config()
 
+app.use(cors());
+
 console.log(process.env.PRUEBA_VARIABLE);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({ msg: 'mensaje desde el backend!!' });
 })
 
 app.listen(port, () => {
